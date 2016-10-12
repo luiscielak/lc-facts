d80be5bd4e2b0f52accece73ecf15d81e149f37d
Luis Cielak <luis.cielak@gmail.com>
2016-10-12 13:22:29 -0400

    cert: Submit skill for certification

--- src/index.js
+++ src/index.js
@@ -8,8 +8,14 @@ var SKILL_NAME = 'LC Facts';
  * Array containing space facts.
  */
 var FACTS = [
-    "Luis is his name.",
-    "Cielak is his last name."
+    "L-U-I-S is his name.",
+    "Cielak is his last name.",
+    "is a User Experience and visualization consultant in Philadelphia.",
+    "his work is focused on the dynamics of engaging people with complex systems.",
+    "designs immersive data visualization and screen-based interfaces.",
+    "holds a BS in Management Information Systems from Florida State University and a MS in Interactive Design and Media from Philadelphia University.",
+    "was born in Mexico City in 1985.",
+    "is a scorpio born in November."
 ];
 
 exports.handler = function(event, context, callback) {
@@ -32,12 +38,12 @@ var handlers = {
         var randomFact = FACTS[factIndex];
 
         // Create speech output
-        var speechOutput = "Here's your fact: " + randomFact;
+        var speechOutput = "So Louis: " + randomFact;
 
         this.emit(':tellWithCard', speechOutput, SKILL_NAME, randomFact)
     },
     'AMAZON.HelpIntent': function () {
-        var speechOutput = "You can say tell me a Luis fact, or, you can say exit... What can I help you with?";
+        var speechOutput = "You can say tell me a Louis fact, or, you can say exit... What can I help you with?";
         var reprompt = "What can I help you with?";
         this.emit(':ask', speechOutput, reprompt);
     },
